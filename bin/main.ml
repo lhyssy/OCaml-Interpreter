326ecaa39c27a9c28da2e2d3b26248645dac6c9a
20161314 <1687699860@qq.com>
2025-08-10 22:14:33 +0800

    暂时取消优化（排查错误），同时测试了部分测试用例，优化代码输出结果。

--- bin/main.ml
+++ bin/main.ml
@@ -70,7 +70,7 @@ let main () =
   if options.show_ast then
     Printf.printf "%s\n" (string_of_program optimized_ast);
 
-  let ir_code = generate_ir optimized_ast in
+  let ir_code = generate_ir ast in
   let ir_code_opt = optimize_program2 ir_code in
   if options.show_ir then
     Printf.printf "%s\n" (string_of_ir_program ir_code_opt);
